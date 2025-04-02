b34e264a0f04cfa823f53b8e941d7f87c0c2126e
joaopaulopereirarezendesesi <joaopp.rezende@gmail.com>
2025-04-02 15:43:35 -0300

    update

--- app.js
+++ app.js
@@ -1,43 +1,34 @@
 const express = require("express");
-const sqlite3 = require("sqlite3");
-const port = 3000;
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-const db = new sqlite3.Database("user.db");
+app.use(bodyParser.json());
 
-db.serialize(() => {
-  db.run(
-    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, celular TEXT, email TEXT, cpf TEXT, rg TEXT)"
-  );
+const cadastroRoutes = require("./routes/rotaCadastro");
+const authRoutes = require("./routes/authRoutes");
+const recoveryPasswordRoutes = require("./routes/recoverPasswordRoutes");
+const fiapRoutes = require("./routes/fiapRoutes");
+
+app.use("/", authRoutes);
+app.use("/recoverPasswordRoutes", recoveryPasswordRoutes);
+app.use("/cadastro", cadastroRoutes);
+app.use("/login", fiapRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello, Node.js!");
 });
 
-app.use("/static", express.static(__dirname + "/static"));
-app.set("view engine", "ejs");
-
-app.get("/cadastro", (req, res) => {
-  res.render("cadastro");
-});
-
-app.post("/cadastro", (req, res) => {
-  console.log("O QUE CHEGOU NO BODY: ", req.body);
-  const { nome, senha, consenha, celular, email, cpf, rg } = req.body;
-
-  if (!nome || !senha || !consenha || !celular || !email || !cpf || !rg) {
-    return res.status(400).json({ erro: "Todos os campos são obrigatórios." });
-  }
-
-  if (senha !== consenha) {
-    return res.status(400).json({ erro: "As senhas não coincidem." });
-  }
-
-  db.run(
-    `INSERT INTO users (username, password, celular, email, cpf, rg) VALUES ("${nome}", "${senha}", "${celular}", "${email}", "${cpf}", "${rg}")`
-  );
-});
-
-app.listen(port, () => {
-  console.log("Servidor em execução na porta 3000");
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
